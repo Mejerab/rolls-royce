eb4f0c763b50826ed2874fde2a37df6b8672c354
Mejerab <omim3670@gmail.com>
2024-10-10 09:38:35 +0600

    rechecked everything

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const useAxiosPublic = () => {
     const axiosPublic = axios.create({
-        baseURL: 'http://localhost:5000',
+        baseURL: 'https://rolls-royce-server.vercel.app',
         withCredentials: true
     })
     return axiosPublic;
